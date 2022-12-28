8608ce202768e4581a93ef537a3a96bb0f6685be
Uicab04 <72843263+Uicab04@users.noreply.github.com>
2022-12-28 18:44:31 -0500

    Add files via upload

---
+++ CHATGPT3.py
@@ -0,0 +1,42 @@
+
+import openai
+import requests
+
+# Tu clave de API de OpenAI va aquí
+openai.api_key = "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"
+
+# Establecemos el modelo que queremos utilizar (en este caso, GPT-3)
+model_engine = "text-davinci-002"
+
+# Creamos la solicitud a la API de OpenAI
+response = openai.Completion.create(
+    engine=model_engine,
+    prompt="Realiza un mapa conceptual del metodo agil con tres conceptos que son ventajas desventajas y estado actual",
+    max_tokens=2048,
+    n=1,
+    temperature=0.5,
+)
+
+# Procesamos la respuesta
+
+response_text = response["choices"][0]["text"]
+
+palabras = response_text.split(" ")
+
+
+# Recorremos cada palabra del texto
+for palabra in palabras:
+    # Si la palabra contiene el símbolo ':', la imprimimos
+    if ":" not in palabra:
+      continue
+    print(palabra)
+
+
+
+
+      
+    
+
+
+
+
